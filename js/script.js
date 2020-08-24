2b6e5535bccc6e36ff1284ed6ab7115e4c9f5662
dom <domepiro87@yahoo.it>
2020-08-24 18:40:43 +0200

    prova

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-alert()
+// chiedi l'email all'utente
+var email_utente = prompt("Inserisci email");
+
+
+// creare email tipo
+var emails = ["dome@gmail.com", "pino@gmail.com", "garo@gmail.com"];
+console.log(emails);
+
+var emailSistema = false;
+
+// controllare che email sia nella lista
+for(var i = 0; i<emails.length; i++){
+  if(email_utente == emails[i]){
+    emailSistema = true;
+    console.log("benvenuto");
+  }
+}
